3acf9be6fb0ddb9864dfaf08b0f43bfa316b3ad5
Parsa Hosseini <parsa@firstsource.tech>
2022-03-14 11:30:48 +0330

    Init commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import * as fs from "fs";
+
+const app = express();
+
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.post("/", async (req, res) => {
+  try {
+    const stringifiedRequest = JSON.stringify(req);
+    const date = Date.now().toString();
+    console.log(date);
+    console.log(stringifiedRequest);
+
+    fs.appendFileSync(`req_${date}.json`, stringifiedRequest);
+  } catch (error) {
+    console.log(error);
+  }
+  res.send({});
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port", process.env.PORT);
+});
